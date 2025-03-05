281f8709cc8a4bef51fe7b5de8e4c6a0071e3bec
Dhruv Patel <dh208315@dal.ca>
2025-03-05 15:33:12 -0400

    Added method for update status

--- Task.java
+++ Task.java
@@ -1,8 +1,13 @@
 package quiz;
 class Task{
     String assigni_name;
+    String statusOfTask;
     public String assignTo(String name){
         assigni_name = name;
         return assigni_name;
+    }
+    public String updateStatus(String status){
+        statusOfTask = status;
+        return statusOfTask; 
     }
 }
